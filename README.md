3f2f31c1a05b8e4056a751a6d271b5809e32b534
mattgd <matt.dzwonczyk@live.com>
2015-05-07 12:30:28 -0400

    Started adding unlimited character support.

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # PasswordSolver
 A basic password solver.
+
+Pseudocode for password solver
+
+if character 2 is 9, move to next character
+
+if character being edited is the last character and it equals 9, 
+change first character to next character, and repeat
--- src/me/mattd/passwordsolver/PasswordSolver.java
+++ src/me/mattd/passwordsolver/PasswordSolver.java
@@ -11,6 +11,7 @@ public class PasswordSolver {
 		System.out.print("Enter a password: ");
 		String password = sc.next();
 		
+		// 62 valid characters to test
 		char[] validChars = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 				'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 
 				'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 
@@ -18,39 +19,76 @@ public class PasswordSolver {
 				'0', '1', '2', '3', '4', '5', '6', '7', '8', '9' };
 		
 		String guess = "";
-		int index = 0, saveIndex = 0, currentChar = 0;
+		int index = 0, firstCharIndex = 0, currentChar = 0, startIndex = 1;
 		while (!guess.equalsIgnoreCase(password)) {
 			
+			// Add characters to match the password length
 			while (guess.length() < password.length()) {
 				guess += 'a';
+				startIndex = password.length() - (password.length() - 1);
+				index = startIndex;
 			}
 			
-			guess = guess.substring(0, index + 1) + validChars[currentChar] + guess.substring(index + 2);
-			currentChar++;
+			// If the current paste character is 9
+			if (currentChar == 61) {
+				currentChar = 0;
+				
+				// If the character being edited is the last character
+				if (isLastCharacter(guess, index)) {
+					
+					// If the first character is not a 9
+					if (firstCharIndex < 61) {
+						guess = validChars[++firstCharIndex] + guess.substring(1); // Change the first character to the next character
+					} else {
+						break;
+					}
+					index = startIndex;
+				} else {
+					index++;
+					guess = validChars[++firstCharIndex] + guess.substring(1);
+				}
+			} else {
+				currentChar++;
+				System.out.printf("%d and %d%n", index, guess.length() - 1);
+				if (index <= guess.length() - 1) {
+					guess = guess.substring(0, index + 1) + validChars[currentChar] + guess.substring(index + 2);
+				} else {
+					guess = guess.substring(0, index + 1) + validChars[currentChar];
+				}
+			}
 			
+			
+
 			System.out.println(guess);
 			
-			if (currentChar == 61) {
+			/*if (currentChar == 62) {
 				currentChar = 0;
 				if (index < guess.length() - 2) {
 					index++;
+					System.out.println(index);
 				} else {
-					if (saveIndex < 61) saveIndex++;
-					guess = validChars[saveIndex] + guess.substring(guess.length() - 1);
+					if (saveIndex < 61) {
+						saveIndex++;
+					} else {
+						saveIndex = 0;
+					}
+					guess = guess.substring(0, index) + validChars[saveIndex] + guess.substring(index + 2);
 				}
 			}
+			
+			try {
+				Thread.sleep(100);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}*/
 		}
 		
 		System.out.printf("The password is '%s'.", guess);
-		
 		sc.close();
 	}
 	
+	public static boolean isLastCharacter(String s, int i) {
+		if (i == s.length() - 1) return true;
+		return false;
+	}
 }
-
-/*
- * aa
- * ab
- * ba
- * bb
- * */
